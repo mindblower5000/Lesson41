ac71abb48976c0f91f5265c08e8b89e6d54f8106
rumpilstilstkin <rumpilstilstkin@gmail.com>
2018-06-21 21:22:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rumpilstilstkin/lesson41/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.rumpilstilstkin.lesson41;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mInfoTextView;
+    private ProgressBar progressBar;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        mInfoTextView = (TextView) findViewById(R.id.tvLoad);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        Button btnLoad = (Button) findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener((v)->onClick2());
+    }
+    public void onClick() {
+        String bestUrl = "https://api.github.com/users";
+        if (!editText.getText().toString().isEmpty()){
+            bestUrl += "/" + editText.getText();
+        }
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkinfo = connectivityManager.getActiveNetworkInfo();
+        if (networkinfo != null && networkinfo.isConnected()) {
+            new DownloadPageTask().execute(bestUrl); // запускаем в новом потоке
+        } else {
+            Toast.makeText(this, "Подключите интернет", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @SuppressLint("StaticFieldLeak")
+    private class DownloadPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mInfoTextView.setText("");
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadOneUrl(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            mInfoTextView.setText(result);
+            progressBar.setVisibility(View.GONE);
+            super.onPostExecute(result);
+        }
+    }
+    private String downloadOneUrl(String address) throws IOException {
+        InputStream inputStream = null;
+        String data = "";
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setReadTimeout(100000);
+            connection.setConnectTimeout(100000);
+            //connection.setRequestMethod("POST");
+            connection.setInstanceFollowRedirects(true);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 200 OK
+                System.out.println("Метод запроса: " +
+                                   connection.getRequestMethod());
+                // Вывести код ответа
+                System.out.println("Ответное сообщение: " +
+                                   connection.getResponseMessage());
+                // Получить список полей и множество ключей из заголовка
+                Map<String, List<String>> myMap = connection.getHeaderFields();
+                Set<String> myField = myMap.keySet();
+                System.out.println("\nДалее следует заголовок:");
+                // Вывести все ключи и значения из заголовка
+                for(String k : myField) {
+                    System.out.println("Ключ: " + k + " Значение: "
+                                       + myMap.get(k));
+                }
+                inputStream = connection.getInputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int read = 0;
+                while ((read = inputStream.read()) != -1) {
+                    bos.write(read);
+                }
+                byte[] result = bos.toByteArray();
+                bos.close();
+                data = new String(result);
+            } else {
+                data = connection.getResponseMessage() + " . Error Code : " + responseCode;
+            }
+            connection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return data;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // OkHttp
+    ///////////////////////////////////////////////////////////////////////////
+
+    OkHttpClient client;
+    HttpUrl.Builder urlBuilder;
+
+    public void onClick2() {
+        client = new OkHttpClient();
+        urlBuilder = HttpUrl.parse("https://api.github.com/users").newBuilder();
+        if (!editText.getText().toString().isEmpty()) {
+            urlBuilder.addQueryParameter("login", editText.getText().toString());
+        }
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkinfo = connectivityManager.getActiveNetworkInfo();
+        if (networkinfo != null && networkinfo.isConnected()) {
+            try {
+                downloadOneUrl(request);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "Подключите интернет", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void downloadOneUrl(Request request) throws IOException {
+        progressBar.setVisibility(View.VISIBLE);
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+                Headers responseHeaders = response.headers();
+                for (int i = 0; i < responseHeaders.size(); i++) {
+                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                }
+                final String responseData = response.body().string();
+                MainActivity.this.runOnUiThread(() -> {
+                    mInfoTextView.setText(responseData);
+                    progressBar.setVisibility(View.GONE);
+                });
+            }
+        });
+    }
+}
+
